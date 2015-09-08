992706fd60cfb27c20cbed8f7ecf99054f827ade
GreenAddress <info@greenaddress.it>
2015-09-08 15:43:02 +0200

    fix makePackage, work with or without changes in tree

--- makePackages.sh
+++ makePackages.sh
@@ -4,8 +4,22 @@ epochTime=`date +%s`
 
 ./prepare.sh testnet
 stashName=`git stash create`;
-git archive -o ../WalletCrxTestNet_$epochTime.zip $stashName
+if [ -z "$stashName" ]
+then
+        echo "Packaging HEAD"
+        git archive -o ../WalletCrxTestNet_$epochTime.zip HEAD
+else
+        echo "Packaging HEAD + localchanges"
+        git archive -o ../WalletCrxTestNet_$epochTime.zip $stashName
+fi
 
 ./prepare.sh mainnet
 stashName=`git stash create`;
-git archive -o ../WalletCrxMainNet_$epochTime.zip $stashName
+if [ -z "$stashName" ]
+then
+        echo "Packaging HEAD"
+        git archive -o ../WalletCrxMainNet_$epochTime.zip HEAD
+else
+        echo "Packaging HEAD + localchanges"
+        git archive -o ../WalletCrxMainNet_$epochTime.zip $stashName
+fi
