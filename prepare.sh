6c7febb9c2d876e11b35553c8d3aa8714c26b711
GreenAddress <info@greenaddress.it>
2016-01-13 22:58:51 +0100

    add network.js, clean config, update prepare, remove  electrum's network permissions, bump version for release

--- prepare.sh
+++ prepare.sh
@@ -16,6 +16,7 @@ then
         if [ -f static/wallet/config_mainnet.js ];
         then
                 cp static/wallet/config_mainnet.js static/wallet/config.js
+                cp static/wallet/network_mainnet.js static/wallet/network.js
                 cp manifest_mainnet.json manifest.json
         fi
         exit 0
@@ -24,10 +25,12 @@ fi
 if [ ! -f static/wallet/config_mainnet.js ];
 then
         cp static/wallet/config.js static/wallet/config_mainnet.js
+        cp static/wallet/network.js static/wallet/network_mainnet.js
         cp manifest.json manifest_mainnet.json
 fi
 
 echo "preparing for $1"
 cp static/wallet/config_$1.js static/wallet/config.js
+cp static/wallet/network_$1.js static/wallet/network.js
 cp manifest_$1.json manifest.json
 exit 0
