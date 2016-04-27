63a9887a75e11375326b64ae75b05424342f5ac8
GreenAddress <info@greenaddress.it>
2016-04-27 16:02:01 +0200

    Removed now-unused btchip firmware upgrade directory

--- btchip_fup/version.js
+++
@@ -1,5 +0,0 @@
-      var CURRENT_BL = (1 << 16) + (3 << 8) + (15);
-      var CURRENT_OS = (1 << 16) + (4 << 8) + (11);
-      var CURRENT_RELOADER = (1 << 16) + (33 << 8) + (0);
-
-      var BETA = false;
