17e931a5d033d7f6ed4fd6791cf462044b85810c
Jerzy Kozera <jerzy.kozera@gmail.com>
2016-09-19 12:41:19 +0200

    Fix makePackages.sh to include langs and static/

--- makePackages.sh
+++ makePackages.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+function add_static {
+        LANGS="de en es fr it pl ru uk sv nl el th"
+        for LANG in $LANGS; do
+                zip -r $1 $LANG
+        done
+        zip -r $1 static/{css,fonts,img,js,sound}
+}
+
 epochTime=`date +%s`
 
-./prepare.sh testnet
+./prepare.sh -s testnet
 stashName=`git stash create`;
 if [ -z "$stashName" ]
 then
         echo "Packaging HEAD"
         git archive -o ../WalletCrxTestNet_$epochTime.zip HEAD
+        add_static ../WalletCrxTestNet_$epochTime.zip
 else
         echo "Packaging HEAD + localchanges"
         git archive -o ../WalletCrxTestNet_$epochTime.zip $stashName
+        add_static ../WalletCrxTestNet_$epochTime.zip
 fi
 
 ./prepare.sh mainnet
@@ -19,7 +29,9 @@ if [ -z "$stashName" ]
 then
         echo "Packaging HEAD"
         git archive -o ../WalletCrxMainNet_$epochTime.zip HEAD
+        add_static ../WalletCrxMainNet_$epochTime.zip
 else
         echo "Packaging HEAD + localchanges"
         git archive -o ../WalletCrxMainNet_$epochTime.zip $stashName
+        add_static ../WalletCrxMainNet_$epochTime.zip
 fi
