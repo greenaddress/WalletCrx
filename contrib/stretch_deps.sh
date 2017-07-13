950bd4437bc9b6927dbab74d7d53d361be33bbd3
Lawrence Nahum <lawrence@greenaddress.it>
2017-07-13 17:43:07 +0200

    upgrade build to stretch and node 8

--- contrib/stretch_deps.sh
+++ contrib/stretch_deps.sh
@@ -4,9 +4,8 @@ set -e
 sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
 apt-get -yqq update && apt-get -yqq upgrade
 apt-get -yqq install zip git curl python-{virtualenv,pip}
-curl -sL https://deb.nodesource.com/setup_7.x | bash -
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get -yqq update && apt-get -yqq install nodejs
-
 
 apt-get remove --purge curl -yqq
 apt-get -yqq autoremove && apt-get -yqq clean
